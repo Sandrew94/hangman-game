d888d72a6f36988740f965f7dc18974d2120034c
Andrea santi <santi@MacBook-Air-001.homenet.telecomitalia.it>
2021-08-15 22:32:13 +0200

    Create a class method that taken a word, it create an html with hidden character; Create a new files for scss variables - Modify async request in the costructor to manipulate the data

--- src/ts/components/asyncRequest.ts
+++ src/ts/components/asyncRequest.ts
@@ -2,15 +2,5 @@ export const takeAPIWord = async function (pullOfWord: number): Promise<void> {
   const response = await fetch(
     `https://random-word-api.herokuapp.com/word?number=${pullOfWord}`
   );
-  const data = await response.json();
-
-  const randomArrNum = Math.floor(Math.random() * (data.length + 1));
-
-  data.map((_: string, idx: number, arr: string[]): undefined | string => {
-    if (idx - 1 !== randomArrNum) return;
-    else {
-      console.log(arr[idx]);
-      return arr[idx];
-    }
-  });
+  return await response.json();
 };
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,9 +2,32 @@ import "../scss/main.scss";
 import { takeAPIWord } from "./components/asyncRequest";
 
 class HangManGame {
-  constructor(public word: Promise<string | void>) {}
+  constructor(public word: Promise<string | void>) {
+    word.then((data: any) => {
+      const randomArrNum = Math.floor(Math.random() * (data.length + 1));
+
+      data.map((_: string, idx: number, arr: string[]): undefined | string => {
+        if (idx - 1 !== randomArrNum) return;
+        console.log(arr[idx]);
+        this.createHtmlKeyword(arr[idx]);
+        return arr[idx];
+      });
+    });
+  }
+
+  createHtmlKeyword(singleWord: string) {
+    const containerLetters = document.querySelector(
+      ".words_container"
+    )! as HTMLDivElement;
+
+    Array.from(singleWord).forEach((_) => {
+      let html = `<span class="words_container-length">*</span>`;
+      containerLetters.insertAdjacentHTML("beforeend", html);
+    });
+  }
 }
 
+// ${el.toUpperCase()}
+
 const randomWord = takeAPIWord(20);
-const hangManNewGame = new HangManGame(randomWord);
-console.log(hangManNewGame);
+new HangManGame(randomWord);
